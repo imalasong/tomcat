d147173ac0e38bdeafb43152939406fa011f2dfa
imalasong <imalasong@qq.com>
2025-01-05 21:36:58 +0800

    add comment

--- java/org/apache/catalina/sample/Main.java
+++ java/org/apache/catalina/sample/Main.java
@@ -5,6 +5,7 @@ import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.coyote.http11.Http11Nio2Protocol;
 import org.apache.coyote.http11.Http11NioProtocol;
 import org.apache.jasper.compiler.JspUtil;
 
@@ -24,25 +25,27 @@ import org.apache.jasper.servlet.JasperInitializer;
 public class Main {
 
     public static final String DEFAULT_PROTOCOL = Http11NioProtocol.class.getName();
+    public static final String DEFAULT_PROTOCOL2 = Http11Nio2Protocol.class.getName();
 
     public static void main(String[] args) throws LifecycleException,
             InterruptedException, ServletException {
 
         Tomcat tomcat = new Tomcat();
 
-        JasperInitializer jasperInitializer = new JasperInitializer();
-
+        //1、配置连接器
         Connector connector = new Connector(DEFAULT_PROTOCOL);
         connector.setPort(8081);
+        //把connector加入Service类中的数组，并且调用connector的start方法
         tomcat.setConnector(connector);
+        Connector connector2 = new Connector(DEFAULT_PROTOCOL2);
+        connector2.setPort(8071);
+        tomcat.setConnector(connector2);
 
-//        Connector connector2 = new Connector(DEFAULT_PROTOCOL);
-//        connector2.setPort(8082);
-//        tomcat.setConnector(connector2);
 
-
+        //2、配置处理器
+        // JSP初始化（非必要项，如果不要jsp的话就不需要下面这行代码）
+        new JasperInitializer();
         String webs = new File("webs").getAbsolutePath();
-
         Context ctx = tomcat.addContext("/",webs );
         tomcat.initWebappDefaults("/");
         Tomcat.addServlet(ctx, "Embedded", new HttpServlet() {
@@ -56,22 +59,10 @@ public class Main {
                 w.close();
             }
         });
-
-        Tomcat.addServlet(ctx, "Embedded2", new HttpServlet() {
-            @Override
-            protected void service(HttpServletRequest req, HttpServletResponse resp)
-                throws ServletException, IOException {
-                Writer w = resp.getWriter();
-                w.write("Embedded Tomcat servlet.\n");
-
-                w.flush();
-                w.close();
-            }
-        });
-
         ctx.addServletMappingDecoded("/hello", "Embedded");
-        ctx.addServletMappingDecoded("/hello2", "Embedded2");
 
+
+        //3、启动tomcat
         tomcat.start();
         System.out.println("Tomcat started at http://localhost:8081/hello");
         tomcat.getServer().await();
